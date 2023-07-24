51edbf98ca834b479f88851ccaf3ee867e0335ff
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-07-24 21:55:11 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import axios from "axios";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState({});
+  const [map, setMap] = useState("");
+
+  function handleSearch(event) {
+    // console.log("Hey you guys!");
+    setSearchQuery(event.target.value);
+    console.log(searchQuery);
+  }
+
+  async function getLocation(event) {
+    try {
+      event.preventDefault();
+      event.target.input.value = "";
+      // console.log("location found");
+      const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
+      const res = await axios.get(API);
+      console.log(res.data[0]);
+      setLocation(res.data[0]);
+      handleMap(res.data[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleMap(data) {
+    // console.log(data);
+    const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data.lat},${data.lon}&zoom=9`;
+    // const res = await axios.get(API);
+    setMap(API);
+  }
+
+  // 52.6285576
+  // 1.2923954
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getLocation}>
+        <input
+          type="text"
+          placeholder="search for a city"
+          name="input"
+          onChange={handleSearch}
+        />
+        <button type="submit">Explore!</button>
+      </form>
+      <p>{location.display_name}</p>
+      {map && <img src={map} alt="map" />}
     </div>
   );
 }
